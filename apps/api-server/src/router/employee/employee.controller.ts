f95b730b7750e476e3ccc09c1bb6368d4c783d6d
Two Silver Ticket <dldmsvy1010@naver.com>
2024-01-04 13:33:55 +0000

    [DEV] employee endpoint api 문서 작성

--- apps/api-server/src/router/employee/employee.controller.ts
+++ apps/api-server/src/router/employee/employee.controller.ts
@@ -9,13 +9,26 @@ import {
 } from '@api-server/type/dto/employee.dto';
 import { Transform } from '@module/common/interceptor/transform.interceptor';
 import { EmployParseIntPipe } from '@api-server/common/pipe/employee.pipe';
+import { ApiBadRequestResponse, ApiOkResponse, ApiOperation, ApiQuery, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { ErrorResponse } from '@module/module/error';
 
 @Controller('employee')
+@ApiTags('employee')
 export class EmployeeController {
     constructor(private readonly employeeService: EmployeeService) {}
 
     @Get()
     @Transform(GetEmployeeResponse)
+    @ApiOperation({
+        summary: 'Empolyee 정보 조회',
+        description: 'employee_id, email, jod_id, department_id 정보를 통해 직원 정보를 조회합니다.',
+    })
+    @ApiQuery({name: 'employee_id', description: '조회하려는 직원 ID', 'example': 100, type: 'number'})
+    @ApiQuery({name: 'email', description: '조회하려는 직원 Email', 'example': 'EMAIL', type: 'string'})
+    @ApiQuery({name: 'job_id', description: '조회하려는 직원의 직업 ID', 'example': 101, type: 'number'})
+    @ApiQuery({name: 'department_id', description: '조회하려는 직원의 부서 ID', 'example': 102, type: 'number'})
+    @ApiOkResponse({type: GetEmployeeResponse, description: '응답 성공'})
+    @ApiBadRequestResponse({type: ErrorResponse , description: '응답 실패'})
     async get(@Query() { employee_id, email, job_id, department_id }: GetEmployeeQueryDto) {
         const employeeList = await this.employeeService.get({ employee_id, email, job_id, department_id });
         return employeeList;
@@ -23,6 +36,13 @@ export class EmployeeController {
 
     @Get('/history')
     @Transform(GetHistoryResponse)
+    @ApiOperation({
+        summary: 'Empolyee 경력 정보 조회',
+        description: '직원 ID를 통해 직원 경력 정보를 조회합니다.',
+    })
+    @ApiQuery({name: 'employee_id', description: '조회하려는 직원 ID', 'example': 100, type: 'number'})
+    @ApiOkResponse({type: GetHistoryResponse, description: '응답 성공'})
+    @ApiBadRequestResponse({type: ErrorResponse , description: '응답 실패'})
     async getHistory(@Query(EmployParseIntPipe) { employee_id }: GetEmployeeHistoryQueryDto) {
         const employeeHistory = await this.employeeService.getHistory({ employee_id });
         return employeeHistory;
--- apps/api-server/src/type/dto/employee.dto.ts
+++ apps/api-server/src/type/dto/employee.dto.ts
@@ -5,70 +5,89 @@ import { IsNumber, IsOptional, IsString, MaxLength } from 'class-validator';
 
 export class EmployeeListResponse {
     @Expose()
+    @ApiProperty({ type: Number, description: '직원 ID', example: 100 })
     employee_id: number;
 
     @Expose()
+    @ApiProperty({ type: String, description: '직원 Email', example: 'EMAIL' })
     email: string;
 
     @Expose()
+    @ApiProperty({ type: String, description: '직원 이름', example: 'EUN PYO' })
     first_name: string;
 
     @Expose()
+    @ApiProperty({ type: String, description: '직원 성', example: 'LEE' })
     last_name: string;
 
     @Expose()
+    @ApiProperty({ type: String, description: '직원 전화번호', example: '123.456.789.0' })
     phone_number: string;
 
     @Expose()
     @Transform(({ value }) => (value ? Number(value) : undefined))
+    @ApiProperty({ type: Number, description: '직원 급여', example: 27000 })
     salary: number;
 
     @Expose()
     @Transform(({ value }) => (value ? Number(value) : undefined))
+    @ApiProperty({ type: Number, description: '커미션 비율', example: 0.4 })
     commission_pct: number;
 
     @Expose({ name: 'jobs' })
     @Transform(({ value }) => value.job_title)
+    @ApiProperty({ type: String, description: '직급', example: 'IT Engineer' })
     job_title: string;
 }
 
 export class GetEmployeeResponse {
     @Expose()
+    @ApiProperty({ type: Number, description: '직원 ID', example: 100 })
     employee_id: number;
 
     @Expose()
+    @ApiProperty({ type: String, description: '직원 Email', example: 'EMAIL' })
     email: string;
 
     @Expose()
+    @ApiProperty({ type: String, description: '직원 이름', example: 'EUN PYO' })
     first_name: string;
 
     @Expose()
+    @ApiProperty({ type: String, description: '직원 성', example: 'LEE' })
     last_name: string;
 
     @Expose()
+    @ApiProperty({ type: String, description: '직원 전화번호', example: '123.456.789.0' })
     phone_number: string;
 
     @Expose()
+    @ApiProperty({ type: Date, description: '직원 고용 날짜', example: new Date() })
     hire_date: Date;
 
     @Expose()
     @Transform(({ obj }) => obj?.departments_employees_department_idTodepartments?.department_id)
+    @ApiProperty({ type: Number, description: '부서 번호', example: 200 })
     department_id: number;
 
     @Expose()
     @Transform(({ obj }) => obj?.departments_employees_department_idTodepartments?.department_name)
+    @ApiProperty({ type: String, description: '부서 이름', example: 'IT' })
     department_name: string;
 
     @Expose({ name: 'jobs' })
     @Transform(({ value }) => value?.job_title)
+    @ApiProperty({ type: String, description: '직급', example: 'IT Engineer' })
     job_title: string;
 
     @Expose()
     @Transform(({ value }) => (value ? Number(value) : undefined))
+    @ApiProperty({ type: Number, description: '직원 급여', example: 27000 })
     salary: number;
 
     @Expose()
     @Transform(({ value }) => (value ? Number(value) : undefined))
+    @ApiProperty({ type: Number, description: '커미션 비율', example: 0.4 })
     commission_pct: number;
 
     @Expose({ name: 'other_employees' })
@@ -80,22 +99,24 @@ export class GetEmployeeQueryDto {
     @IsOptional()
     @IsNumber()
     @Transform(({ value }) => (value === undefined ? undefined : +value))
-    @ApiProperty()
+    @ApiProperty({ type: Number, description: '직원 ID', example: 100 })
     employee_id?: number;
 
     @IsOptional()
     @IsString()
+    @ApiProperty({ type: String, description: '직원 Email', example: 'EMAIL' })
     email?: string;
 
     @IsOptional()
     @IsString()
     @MaxLength(10)
+    @ApiProperty({ type: String, description: '직급 ID', example: 'JOB_ID' })
     job_id?: string;
 
     @IsOptional()
     @IsNumber()
     @Transform(({ value }) => (value === undefined ? undefined : +value))
-    @ApiProperty()
+    @ApiProperty({ type: Number, description: '부서 ID', example: 200 })
     department_id?: number;
 }
 
@@ -103,31 +124,39 @@ export class GetEmployeeHistoryQueryDto extends PickType(GetEmployeeQueryDto, ['
 
 export class HistoryList {
     @Expose()
+    @ApiProperty({type: Date, description: '입사일', example: new Date()})
     start_date: Date;
 
     @Expose()
+    @ApiProperty({type: Date, description: '퇴사일', example: new Date()})
     end_date: Date;
 
     @Expose({ name: 'departments' })
     @Transform(({ value }) => value.department_name)
+    @ApiProperty({type: String, description: '부서명', example: 'IT'})
     department_name: string;
 
     @Expose({ name: 'jobs' })
     @Transform(({ value }) => value.job_title)
+    @ApiProperty({type: String, description: '직급', example: 'IT Engineer'})
     job_title: string;
 }
 
 export class GetHistoryResponse {
     @Expose({ name: 'id' })
+    @ApiProperty({ type: Number, description: '직원 ID', example: 100 })
     employee_id: number;
 
     @Expose()
+    @ApiProperty({ type: String, description: '직원 Email', example: 'EMAIL' })
     email: string;
 
     @Expose()
+    @ApiProperty({ type: String, description: '직원 이름', example: 'EUN PYO' })
     first_name: string;
 
     @Expose()
+    @ApiProperty({ type: String, description: '직원 성', example: 'LEE' })
     last_name: string;
 
     @Expose({ name: 'historyList' })
@@ -137,11 +166,6 @@ export class GetHistoryResponse {
 
 export class GetHistoryQueryDto {
     @IsNumber()
+    @ApiProperty({ type: Number, description: '직원 ID', example: 100 })
     employee_id: number;
 }
-
-export class GetLocationQueryDto {}
-
-export class UpdateSalaryBodyDto {}
-
-export class UpdateEmployBodyDto {}
